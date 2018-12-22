4ee165fd1a2bf3a5bed88d98f5fe2fadd2140a3c
Kushal <kushaldeveloper@gmail.com>
2018-12-22 10:10:36 -0500

    add everything 20181222101036

--- upload.sh
+++ upload.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-cp /home/kus/Videos/obs/*.mp4 /home/kus/src/personal/30s/public/video/;
+cp /home/kus/Videos/obs/*.mp4 /home/kus/src/personal/30s;
 cd /home/kus/src/personal/30s || exit;
 git add .;
 git commit -m "add everything `date +%Y%m%d%H%M%S`";

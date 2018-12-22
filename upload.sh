5b9611113463fa6a0aa9494f5e64867e8272ce22
Kushal <kushaldeveloper@gmail.com>
2018-12-22 10:02:04 -0500

    add everything 20181222100204

--- upload.sh
+++ upload.sh
@@ -3,6 +3,6 @@
 cp /home/kus/Videos/obs/*.mp4 /home/kus/src/personal/30s/public/video/;
 cd /home/kus/src/personal/30s || exit;
 git add .;
-git commit -m "add everything `date +%Y%m%d`";
+git commit -m "add everything `date +%Y%m%d%H%M%S`";
 git pull --strategy-option=theirs;
 git push origin master;
